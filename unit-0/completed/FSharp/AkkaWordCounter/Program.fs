d0750f69617427e4888afe1d244b584b9e55dae0
Henrik Kirk <henrikkirk@gmail.com>
2025-06-02 21:22:17 +0200

    Init actorsystem

---
+++ unit-0/completed/FSharp/AkkaWordCounter/Program.fs
@@ -0,0 +1,9 @@
+﻿open Akka.Actor
+open Akka.Event
+open Akka.FSharp
+
+let myActorSystem = System.create "LocalSystem" (Configuration.load())
+
+myActorSystem.Log.Info "Helle from the actor system"
+
+myActorSystem.WhenTerminated.Wait ()
